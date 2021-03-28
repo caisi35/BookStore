dbe38fb44148b12105ef1ff3c37aa338cfc92607
caisi <caisi1735@163.com>
2021-03-28 08:32:02 +0800

    change

--- spider/dangdangbook3_bs4.py
+++ spider/dangdangbook3_bs4.py
@@ -3,12 +3,36 @@ import requests
 from bs4 import BeautifulSoup
 import time
 import pymongo
-from utils import mail
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+
+my_sender = 'caisi-huang@139.com'  # 发件人邮箱账号
+my_pass = 'Asdf17135'  # 发件人邮箱密码
+my_user = 'caisi1735@163.com'  # 收件人邮箱账号，我这边发送给自己
 
 HEADER = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
                         " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36"}
 LOGGER = logging.basicConfig(filename='dangdangbook3_bs4.log', level=logging.INFO)
 CONN = pymongo.MongoClient(host='mongo', port=27017, username='root', password='root').get_database('bookstore')
+
+
+def mail(title='', content=''):
+    ret = True
+    try:
+        msg = MIMEText(content, 'plain', 'utf-8')
+        msg['From'] = formataddr(["Caisi", my_sender])  # 括号里的对应发件人邮箱昵称、发件人邮箱账号
+        msg['To'] = formataddr(["To 163", my_user])  # 括号里的对应收件人邮箱昵称、收件人邮箱账号
+        msg['Subject'] = title  # 邮件的主题，也可以说是标题
+
+        server = smtplib.SMTP("smtp.139.com", 25)  # 发件人邮箱中的SMTP服务器，端口是25
+        server.login(my_sender, my_pass)  # 括号中对应的是发件人邮箱账号、邮箱密码
+        server.sendmail(my_sender, [my_user, ], msg.as_string())  # 括号中对应的是发件人邮箱账号、收件人邮箱账号、发送邮件
+        server.quit()  # 关闭连接
+    except Exception as e:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+        ret = False
+        print(e)
+    return ret
 
 
 def get_category_url(category_url):
